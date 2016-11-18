f1a9ee02c96787872a84f519f59e63112a474afd
Laketek <samuelanjum@hotmail.com>
2016-11-18 19:24:29 +0000

    textfield + keyboard changes
    textfield changed to readonly
    keyboard inputs to textfield without clicking it
    enter key finds answer

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 function backspace() {
-  var value = document.getElementById("ans").value;
-  document.getElementById("ans").value = value.substr(0, value.length - 1);
+  document.getElementById("ans").value = document.getElementById("ans").value.substr(0, document.getElementById("ans").value.length - 1);
 }
 
 function evalAnswer() {
   document.getElementById("ans").value = math.eval(document.getElementById("ans").value);
 }
+
+document.addEventListener('keypress', function(event) {
+  if(event.which === 13) {
+    event.preventDefault();
+    document.getElementById("ans").value = math.eval(document.getElementById("ans").value);
+  }
+  else {
+    document.getElementById("ans").value += String.fromCharCode(event.keyCode);
+  }
+});
+
+document.addEventListener('keydown', function(event) {
+  if(event.which === 8) {
+    backspace();
+  }
+});
